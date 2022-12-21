d0edcd981e21ba0706057169b745fabc0804478f
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-21 10:50:22 +0100

    aggiungo index.php sistemato

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once __DIR__ . '/classi/generi.php';
+include_once __DIR__ . '/classi/libro.php';
+include_once __DIR__ . '/classi/audiolibro.php';
+
+
+$generi = [
+  'giallo' => new Generi('giallo', 'icon-font-awesome-giallo'),
+  'horror' => new Generi('horror', 'icon-font-awesome-horror')
+];
+var_dump( $generi );
+
+$prodotti = [
+  new Libro( 'La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 250, 'flessibile' ),
+  new audioLibro('La scelta di Natan', 'Antonio Puccio', 23.40, $generi['giallo'], true, 'https://immagine-copertina.com', 120, 'spotify', 'https://spotify-libri.com'),
+];
+
+var_dump( $prodotti );
+
+foreach( $prodotti as $elem ){
+  echo get_class($elem);
+  echo "<br>";
+}
+?>
