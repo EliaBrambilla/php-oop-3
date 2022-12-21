24e46446e471c249e3dd2c70456ec2141e308938
EliaBrambilla <elia.brambilla@icloud.com>
2022-12-21 10:48:47 +0100

    aggiungo generi.php

---
+++ classi/generi.php
@@ -0,0 +1,17 @@
+<?php
+
+class Generi{
+  public $name;
+  public $icon;
+
+  public function __construct(
+    String $name,
+    String $icon
+    ) {
+    $this->name = $name;
+    $this->icon = $icon;
+  }
+
+}
+
+?>
